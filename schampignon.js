7acaf339a871444fc2ad95a6cd72e4d188d84848
Manuel Simoni <msimoni@gmail.com>
2011-08-19 19:51:20 +0200

    fexprs, man

--- schampignon.js
+++ schampignon.js
@@ -1,23 +1,9 @@
 // By Manuel Simoni <msimoni@gmail.com>, August 2011
 // In the public domain.
 
-// An interpreter with tail-call elimination and multi-shot
-// continuations modelled after Kent Dybvig's dissertation
-// ``Three Implementation Models for Scheme'', section 3.4.
-// http://www.cs.indiana.edu/~dyb/pubs/3imp.pdf
+// An interpreter for a very minimal, Kernel-like language:
 //
-// Examples:
-// ["set!", "x", 1]
-// "x" --> 1
-//
-// ["call/cc", ["lambda", ["k"], ["k", 12]]] --> 12
-//
-// ["set!", "foo", ["lambda", [], ["bar"]]]
-// ["set!", "bar", ["lambda", [], 44]]
-// ["foo"] --> 44
-//
-// Differences:
-// - Arguments are evaluated left-to-right instead of right-to-left.
+// [["$vau", "x", "%ignore", "x"], 1, 2] --> [1, 2]
 
 /**** Virtual Machine ****/
 
@@ -29,8 +15,6 @@ function Scm_vm()
     this.x = null;
     // Environment
     this.e = scm_make_env();
-    // aRguments
-    this.r = scm_make_args();
     // Stack
     this.s = null;
 }
@@ -43,31 +27,19 @@ function scm_compile(form, next)
         return { op: "refer", name: form, next: next };
     } else if (scm_is_compound(form)) {
         switch(scm_compound_operator(form)) {
-        case "quote":
-            var obj = scm_compound_elt(form, 1);
-            return { op: "constant", obj: obj, next: next };
-        case "lambda":
-            var vars = scm_compound_elt(form, 1);
-            var body = scm_compound_elt(form, 2);
+        case "$vau":
+            var sig = scm_compound_elt(form, 1);
+            var denv = scm_compound_elt(form, 2);
+            var body = scm_compound_elt(form, 3);
             return { op: "close",
-                     vars: vars,
+                     sig: sig,
+                     denv: denv,
                      body: scm_compile(body, { op: "return" }),
                      next: next };
-        case "if":
-            var test = scm_compound_elt(form, 1);
-            var consequent = scm_compound_elt(form, 2);
-            var alternative = scm_compound_elt(form, 3);
-            var consequent_insn = scm_compile(consequent, next);
-            var alternative_insn = scm_compile(alternative, next);
-            return scm_compile(test, { op: "test",
-                                       consequent: consequent_insn,
-                                       alternative: alternative_insn });
-        case "set!":
+        case "$define!":
             var name = scm_compound_elt(form, 1);
             var value = scm_compound_elt(form, 2);
             return scm_compile(value, { op: "assign", name: name, next: next });
-        case "call/cc":
-            return scm_compile_callcc(form, next);
         default:
             return scm_compile_application(form, next);
         }
@@ -76,38 +48,14 @@ function scm_compile(form, next)
     }
 }
 
-function scm_compile_callcc(form, next)
+function scm_compile_application(form, next)
 {
-    var f = scm_compound_elt(form, 1);
-    var c = { op: "conti",
-              next: { op: "argument",
-                      next: scm_compile(f, { op: "apply" }) } };
+    var f = scm_compound_elt(form, 0);
+    var c = scm_compile(f, { op: "apply", form: form.slice(1) });
     if (scm_is_return(next)) {
         return c;
     } else {
         return { op: "frame", ret: next, next: c };
-    }
-}
-
-function scm_compile_application(form, next)
-{
-    var f = scm_compound_elt(form, 0);
-    return compile_application(scm_compile(f, { op: "apply" }),
-                               scm_compound_length(form) - 1);
-
-    function compile_application(c, i)
-    {
-        if (i === 0) {
-            if (scm_is_return(next)) {
-                return c;
-            } else {
-                return { op: "frame", ret: next, next: c };
-            }
-        } else {
-            return compile_application(scm_compile(scm_compound_elt(form, i),
-                                                   { op: "argument", next: c }),
-                                       i - 1);
-        }
     }
 }
 
@@ -135,54 +83,28 @@ function scm_eval(vm, form)
             vm.x = insn.next;
             continue;
         case "close":
-            vm.a = scm_make_closure(insn.body, vm.e, insn.vars);
+            vm.a = scm_make_closure(insn.body, vm.e, insn.sig, insn.denv);
             vm.x = insn.next;
-            continue;
-        case "test":
-            vm.x = vm.a ? insn.consequent : insn.alternative;
             continue;
         case "assign":
             scm_update(vm.e, insn.name, vm.a);
             vm.x = insn.next;
             continue;
-        case "conti":
-            vm.a = scm_make_continuation(vm.s);
-            vm.x = insn.next;
-            continue;
-        case "nuate":
-            vm.a = scm_lookup(vm.e, insn.name);
-            vm.x = { op: "return" };
-            vm.s = insn.s;
-            continue;
         case "frame":
             vm.x = insn.next;
-            vm.r = scm_make_args();
-            vm.s = scm_make_frame(insn.ret, vm.e, vm.r, vm.s);
-            continue;
-        case "argument":
-            scm_push_arg(vm.r, vm.a);
-            vm.x = insn.next;
+            vm.s = scm_make_frame(insn.ret, vm.e, vm.s);
             continue;
         case "apply":
             vm.x = vm.a.body;
-            vm.e = scm_extend(vm.a.e, vm.a.vars, vm.r);
-            vm.r = scm_make_args();
+            vm.e = scm_extend(vm.a.e, vm.a.sig, vm.a.denv, insn.form);
             continue;
         case "return":
             vm.x = vm.s.x;
             vm.e = vm.s.e;
-            vm.r = vm.s.r;
             vm.s = vm.s.s;
             continue;
         }
     }
-}
-
-function scm_make_continuation(s)
-{
-    return scm_make_closure({ op: "nuate", name: "v", s: s },
-                            scm_make_env(),
-                            ["v"]);
 }
 
 /**** Environments ****/
@@ -228,50 +150,39 @@ function scm_just_update(env, name, value)
     env.bindings[name] = value;
 }
 
-function scm_extend(env, vars, args)
+function scm_extend(env, sig, denv, form)
 {
     var xenv = scm_make_env(env);
-    for (var i = 0; (i < vars.length) && (i < args.length); i++) {
-        scm_just_update(xenv, vars[i], args[i]);
-    }
+    scm_just_update(xenv, sig, form);
+    scm_just_update(xenv, denv, env);
     return xenv;
 }
 
 /**** Closures, Call Frames, Arguments ****/
 
-function Scm_closure(body, env, vars)
+function Scm_closure(body, env, sig, denv)
 {
     this.body = body;
     this.e = env;
-    this.vars = vars;
+    this.sig = sig;
+    this.denv = denv;
 }
 
-function scm_make_closure(body, env, args)
+function scm_make_closure(body, env, args, denv)
 {
-    return new Scm_closure(body, env, args);
+    return new Scm_closure(body, env, args, denv);
 }
 
-function Scm_frame(x, e, r, s)
+function Scm_frame(x, e, s)
 {
     this.x = x;
     this.e = e;
-    this.r = r;
     this.s = s;
 }
 
-function scm_make_frame(x, e, r, s)
+function scm_make_frame(x, e, s)
 {
-    return new Scm_frame(x, e, r, s);
-}
-
-function scm_make_args()
-{
-    return [];
-}
-
-function scm_push_arg(args, value)
-{
-    args.push(value);
+    return new Scm_frame(x, e, s);
 }
 
 /**** Forms ****/
